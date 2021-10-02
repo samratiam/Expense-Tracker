fc49b8ac409af91fb600f0ecb422ab0e2b253229
Samrat Pudasaini <pudasaini.samrat@gmail.com>
2021-10-02 11:01:46 +0545

    Expenses component updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ExpenseItem from "./components/ExpenseItem";
+
 import "./components/ExpenseItem.css";
+import "./components/Expenses.css";
+import Expenses from "./components/Expenses";
 
 const App = () => {
   const expenses = [
@@ -31,26 +33,7 @@ const App = () => {
   ];
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 };
